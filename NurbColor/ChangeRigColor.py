b77d73fca1394e0963414b7f40b288c8507b185a
Melia Masek <Masekmelia@gmail.com>
2022-12-07 10:08:46 -0700

    added nurbcolor script

---
+++ NurbColor/ChangeRigColor.py
@@ -0,0 +1,23 @@
+import maya.cmds as cmds
+
+#Colors are from 0-31
+def ChangeColor(ColorInt):
+    selection = cmds.ls(selection = True)
+
+    for select in selection:
+        cmds.setAttr(select + ".overrideEnabled", True)
+        cmds.setAttr(select + ".overrideColor", ColorInt)
+ChangeColor(13)
+
+
+
+#function for color change with names, notes from class
+#def ChangeNurbColor(color):
+#       if color == "red":
+#           color  = 13
+#       if color > 31:
+#           color = 0
+#       else:
+#           color == 0
+#ChangeColor("red")
+
